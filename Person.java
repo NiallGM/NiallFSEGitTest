794517beac5a963a3436e749f108f7f38bc14b4b
Niall McLaren <niallgmclaren@gmail.com>
2021-11-18 09:50:39 +0000

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,22 @@
+package fsestuff.usefulclasses;
+
+public class Person
+{
+private String firstName
+private String surname
+private String other
+
+	public person ()
+	{
+		firstName = "";
+		surname = "";
+		other = "";
+
+	}
+	public Person(String input1, String input2)
+	{
+		firstName = input1;
+		surname = input2;
+		other = "";
+	}
+}
